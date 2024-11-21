00f0901f040515e8c85a640f39f38560860e8697
Arun Ashok <98117748+arunashokset@users.noreply.github.com>
2024-11-21 23:10:43 +0530

    replacing all JS files - commit -3
    replacing all JS files - commit -3

---
+++ js/main.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+
+
+
+	
+  $("div.blog-post").hover(
+    function() {
+        $(this).find("div.content-hide").slideToggle("fast");
+    },
+    function() {
+        $(this).find("div.content-hide").slideToggle("fast");
+    }
+  );
+
+  $('.flexslider').flexslider({
+    animation: "slide",
+    slideshowSpeed: 5000,
+    prevText: '',
+    nextText: '',
+    controlNav: true,
+    directionNav: true,
+    start: function () {
+        console.log("FlexSlider initialized successfully!");
+    },
+    // after: function (slider) {
+    //     // Hide all captions
+    //     $('.slider-caption').css({ opacity: 0, visibility: 'hidden' });
+
+    //     // Show caption for the active slide
+    //     $(slider.slides[slider.currentSlide])
+    //         .find('.slider-caption')
+    //         .css({ opacity: 1, visibility: 'visible' });
+    // }
+});
+
+  $('.testimonails-slider').flexslider({
+    animation: 'slide',
+    slideshowSpeed: 5000,
+    prevText: '',
+    nextText: '',
+    controlNav: false
+  });
+
+  $(function(){
+
+  // Instantiate MixItUp:
+
+  $('#Container').mixItUp();
+
+  
+
+  $(document).ready(function() {
+      $(".fancybox").fancybox();
+    });
+
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    if (window.innerWidth > 992) {
+        document.querySelectorAll('.navbar .dropdown').forEach(function (dropdown) {
+            dropdown.addEventListener('mouseover', function () {
+                const menu = this.querySelector('.dropdown-menu');
+                menu.classList.add('show');
+            });
+            dropdown.addEventListener('mouseleave', function () {
+                const menu = this.querySelector('.dropdown-menu');
+                menu.classList.remove('show');
+            });
+        });
+    }
+});
+
+
+});
+
