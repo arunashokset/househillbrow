b22b0d060c4cf0b0682979a6351f35b82cdaab41
Arun Ashok <98117748+arunashokset@users.noreply.github.com>
2024-11-23 18:49:18 +0530

    mobile toggle button alignemnt corrected

---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+curl -X POST -d "name=John&email=househillbrow@gmail.com&phone=1234567890&location=NewYork&message=Hello" https://arunashokset.github.io/househillbrow/contact-us.html
+echo "Form submitted successfully.";
+?>
