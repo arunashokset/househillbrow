472b7808437d1719f99439d74e70842f1f250f9d
Arun Ashok <98117748+arunashokset@users.noreply.github.com>
2024-12-06 10:14:04 +0530

    contact mail

--- contact.php
+++ contact.php
@@ -1,55 +1,19 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+<?php 
+if(isset($_POST['submit'])){
+    $to = "arunchennangattussery@gmail.com";
+    $from = $_POST['email']; 
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $location = $_POST['location'];
+    $subject = "HouseHillbrow Enquiry";
+    $message = $fullname . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $fullname . "\n\n" . $_POST['message'];
 
-    // Receiver's email address
-    $email_to = "househillbrow@gmail.com";
-    $email_subject = "HILLBROW - New Enquiry";
-
-    // Validation: Check required fields
-    if (
-        empty($_POST['name']) ||
-        empty($_POST['email']) ||
-        empty($_POST['phone']) ||
-        empty($_POST['location']) ||
-        empty($_POST['message'])
-    ) {
-        die('Error: All fields are required.');
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$phone,$email,$message,$headers);
+    // mail($from,$subject2,$message2,$headers2); 
+    echo "Mail Sent. Thank you " . $fullname . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
-
-    // Assign POST values
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    $email_from = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
-    $location = filter_var($_POST['location'], FILTER_SANITIZE_STRING);
-    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-
-    // Validate email
-    if (!filter_var($email_from, FILTER_VALIDATE_EMAIL)) {
-        die('Error: Invalid email address.');
-    }
-
-    // Prepare the email content
-    $email_message = "New enquiry details:\n\n";
-    $email_message .= "Name: " . $name . "\n";
-    $email_message .= "Email: " . $email_from . "\n";
-    $email_message .= "Phone: " . $phone . "\n";
-    $email_message .= "Location: " . $location . "\n";
-    $email_message .= "Message: " . $message . "\n";
-
-    // Email headers
-    $headers = "From: " . $email_from . "\r\n" .
-               "Reply-To: " . $email_from . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
-
-    // Send the email
-    if (@mail($email_to, $email_subject, $email_message, $headers)) {
-        // Redirect to a thank-you page upon success
-        header('Location: thank-you.html');
-        exit;
-    } else {
-        echo "<p style='color: red;'>Oops, something went wrong. Please try again later.</p>";
-    }
-} else {
-    die('Error: Invalid request method.');
-}
 ?>
